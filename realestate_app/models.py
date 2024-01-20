ad88aeb3637f5d037b1535b3a4645a1c4e3b899e
keith-kay <keithrhova@gmail.com>
2024-01-20 12:14:00 +0300

    model edit commit

--- realestate_app/models.py
+++ realestate_app/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-def user:
-    pass
+class land(models.Model):
+    title = models.CharField(max_length = 200)
+    location = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    dimensions = models.CharField(max_length=200)
+    video_file = models.FileField(upload_to='videos/')
+    
+    def __str__(self):
+        return self.title
