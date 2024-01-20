819c91584dc84a2dc40a27a039090c05751bce8e
keith-kay <keithrhova@gmail.com>
2024-01-20 13:00:02 +0300

    final commit

--- realestate_app/admin.py
+++ realestate_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Land, Property, Property_type
 # Register your models here.
+class LandAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Land)
+admin.site.register(Property)
+admin.site.register(Property_type)
--- realestate_app/models.py
+++ realestate_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class land(models.Model):
+class Land(models.Model):
     title = models.CharField(max_length = 200)
     location = models.CharField(max_length=200)
     description = models.TextField()
@@ -12,3 +12,25 @@ class land(models.Model):
     
     def __str__(self):
         return self.title
+  
+class Property_type(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name 
+    
+class Property(models.Model):
+    title = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.CharField(max_length=200)
+    payment_plan = models.CharField(max_length=200)
+    amenities = models.TextField()
+    property_type = models.ForeignKey(Property_type, on_delete=models.CASCADE )
+    photo = models.ImageField(upload_to='property_photos/', null=True)
+    is_available = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.title 
+    
